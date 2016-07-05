a61bb4a01178e665abb9354605322d42acda4ac3
Kent C. Dodds <kent@doddsfamily.us>
2016-07-05 00:23:35 -0600

    add offline-plugin

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import 'todomvc-app-css/index.css'
 import './app.css'
+import {install as offlineInstall} from 'offline-plugin/runtime'
 
 import {$on} from './helpers'
 import {updateTodo} from './todo'
 import toggleGraph from './graph'
+
+if (process.env.NODE_ENV === 'production') {
+  offlineInstall()
+}
 
 export function onLoad() { // eslint-disable-line import/prefer-default-export
   updateTodo()
